c3280a0f1865b510f0678ca9ee79352bf2d7d5c4
Bohdan Zaichenko <ichim.akih1r0@gmail.com>
2020-12-12 21:18:35 +0100

    Second commit

--- js/script.js
+++ js/script.js
@@ -13,15 +13,15 @@ const reactBar = document.querySelector('.bar-react')
 
 var t1 = new TimelineLite()
 
-t1.fromTo(htmlBar, .75, { width: `calc(0% - 6px)` }, { width: `calc(90% - 6px)` }, ease: Power4.easeOut)
-    .fromTo(cssBar, .75, { width: `calc(0% - 6px)` }, { width: `calc(95% - 6px)` }, ease: Power4.easeOut)
-    .fromTo(jsBar, .75, { width: `calc(0% - 6px)` }, { width: `calc(75% - 6px)` }, ease: Power4.easeOut)
-    .fromTo(reactBar, .75, { width: `calc(0% - 6px)` }, { width: `calc(70% - 6px)` }, ease: Power4.easeOut)
+t1.fromTo(htmlBar, .75, { width: `calc(0% - 6px)` }, { width: `calc(70% - 6px)` , ease: Power4.easeOut})
+    .fromTo(cssBar, .75, { width: `calc(0% - 6px)` }, { width: `calc(60% - 6px)` , ease: Power4.easeOut})
+    .fromTo(jsBar, .75, { width: `calc(0% - 6px)` }, { width: `calc(50% - 6px)` , ease: Power4.easeOut})
+    .fromTo(reactBar, .75, { width: `calc(0% - 6px)` }, { width: `calc(80% - 6px)` , ease: Power4.easeOut})
 
 const controller = new ScrollMagic.Controller()
 const scene = new ScrollMagic.Scene({
     triggerElement: '.skills',
-    triggerHook: 0
+    triggerHook: 0.25
 })
     .setTween(t1)
     .addTo(controller)
@@ -36,15 +36,15 @@ const showRequiredCategory = event => {
     }
 
     event.classList.add('active')
-    const getCategory = document.querySelector('.category-${getId}')
+    const getCategory = document.querySelector(`.category-${getId}`)
     const categories = document.querySelectorAll('div[class ^= "category-"]')
     for (i = 0; i < categories.length; i++) {
         if (categories[i].hasAttribute('class')) {
-            categories[i].classList.remove("showCategory")
+            categories[i].classList.remove('showCategory')
             categories[i].classList.add('hideCategory')
         }
     }
 
     getCategory.classList.remove('hideCategory')
-    getCategory.classList.remove('showCategory')
+    getCategory.classList.add('showCategory')
 }
